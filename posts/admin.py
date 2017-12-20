2aa263bdce5dc282dc08d98c3ff523f2a248f1f3
longhairguy <abhinavrawat2308@gmail.com>
2017-12-20 14:18:50 +0530

    In App Urls

--- posts/admin.py
+++ posts/admin.py
@@ -5,6 +5,15 @@ from django.contrib import admin
 #Used to setup our Post app with the admin panel
 from .models import Post
 
+class PostModelAdmin(admin.ModelAdmin):
+
+    list_display = ['title','updated','timestamp']
+    list_display_links = ['updated']
+    list_filter = ['title']
+    search_fields = ['title','content']
+    class Meta:
+        model = Post
+
 
 #admin function to register model with admin site
-admin.site.register(Post)
+admin.site.register(Post,PostModelAdmin)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# FUNCTION BASED VIEWS
+
+def post_create(request):
+    return HttpResponse("<h1>Hello from Function based Views</h1>")
+
+def post_detail(request):
+    return HttpResponse("<h1>Hello from Function based Views</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>Hello from Function based Views</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Hello from Function based Views</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Hello from Function based Views</h1>")
